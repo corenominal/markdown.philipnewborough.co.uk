0d8d2ccd22334d363581df0a551c1d65cb60ae4e
Philip Newborough <corenominal@corenominal.org>
2026-03-16 20:14:41 +0000

    No admin required for this site

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,11 +7,6 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-// Admin routes
-$routes->get('/admin', 'Admin\Home::index');
-$routes->get('/admin/datatable', 'Admin\Home::datatable');
-$routes->post('/admin/delete', 'Admin\Home::delete');
-
 // API routes
 $routes->match(['get', 'options'], '/api/test/ping', 'Api\Test::ping');
 
